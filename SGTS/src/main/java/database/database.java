351853903cdd085ddaa02cdac456125e134e9dbd
hamdi-ab <hamdiabdulfetah22@gmail.com>
2023-12-21 16:43:54 +0300

    database update

--- SGTS/src/main/java/database/database.java
+++ SGTS/src/main/java/database/database.java
@@ -27,18 +27,25 @@ public class database {
                     "StudentID INTEGER PRIMARY KEY," +
                     "FirstName TEXT NOT NULL," +
                     "LastName TEXT NOT NULL," +
-                    "Age INTEGER," +
+                    "Sex TEXT," +
                     "DepartmentID INTEGER," +
                     " FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)" +
                     ")";
             state.executeUpdate(createStudentsTable);
 
+            String createSemesterTable = "CREATE TABLE IF NOT EXISTS Semester (" +
+                    "SemesterID INTEGER PRIMARY KEY," +
+                    "SemesterName TEXT" +
+                    ")";
+
+            state.executeUpdate(createSemesterTable);
+
             String createCoursesTable = "CREATE TABLE IF NOT EXISTS Courses (" +
-                    "CourseID INTEGER PRIMARY KEY," +
+                    "CourseID TEXT PRIMARY KEY," +
                     "CourseName TEXT NOT NULL," +
                     "Credits INTEGER," +
-                    "DepartmentID INTEGER," +
-                    " FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)" +
+                    "SemesterID INTEGER," +
+                    " FOREIGN KEY (SemesterID) REFERENCES Semester(SemesterID)" +
                     ")";
             state.executeUpdate(createCoursesTable);
 
@@ -46,7 +53,7 @@ public class database {
                     "TeacherID INTEGER PRIMARY KEY," +
                     "FirstName TEXT NOT NULL," +
                     "LastName TEXT NOT NULL," +
-                    "Age INTEGER," +
+                    "Sex TEXT," +
                     "DepartmentID INTEGER," +
                     " FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)" +
                     ")";
@@ -59,24 +66,6 @@ public class database {
             throw new RuntimeException(ex);
         }
     }
-    public static  void insertData(int id, String fName,String lName, int age)  {
-        try {
-            Connection con = DriverManager.getConnection(url);
-            //Statement state = con.createStatement();
-            String insertDataSQL = "INSERT INTO Students (StudentID, FirstName, LastName, Age) VALUES (?,?,?,?)";
-            PreparedStatement preparedStatement = con.prepareStatement(insertDataSQL);
-            preparedStatement.setInt(1, id);
-            preparedStatement.setString(2, fName);
-            preparedStatement.setString(3, lName);
-            preparedStatement.setInt(4, age);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
 
     private static void insertDepartmentData(Connection connection, int departmentID, String departmentName) {
         String insertDataSQL = "INSERT INTO Department (DepartmentID, DepartmentName) VALUES (?, ?)";
@@ -89,13 +78,13 @@ public class database {
         }
     }
 
-    private static void insertStudentData(Connection connection, int studentID, String firstName, String lastName, int age, int departmentID) {
-        String insertDataSQL = "INSERT INTO Students (StudentID, FirstName, LastName, Age, DepartmentID) VALUES (?, ?, ?, ?, ?)";
+    private static void insertStudentData(Connection connection, int studentID, String firstName, String lastName, String sex, int departmentID) {
+        String insertDataSQL = "INSERT INTO Students (StudentID, FirstName, LastName, Sex, DepartmentID) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertDataSQL)) {
             preparedStatement.setInt(1, studentID);
             preparedStatement.setString(2, firstName);
             preparedStatement.setString(3, lastName);
-            preparedStatement.setInt(4, age);
+            preparedStatement.setString(4, sex);
             preparedStatement.setInt(5, departmentID);
             preparedStatement.executeUpdate();
         } catch (Exception e) {
@@ -103,13 +92,13 @@ public class database {
         }
     }
 
-    private static void insertCourseData(Connection connection, int courseID, String courseName, int credits, int departmentID) {
-        String insertDataSQL = "INSERT INTO Courses (CourseID, CourseName, Credits, DepartmentID) VALUES (?, ?, ?, ?)";
+    private static void insertCourseData(Connection connection, String courseID, String courseName, int credits, int SemesterID) {
+        String insertDataSQL = "INSERT INTO Courses (CourseID, CourseName, Credits, SemesterID) VALUES (?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertDataSQL)) {
-            preparedStatement.setInt(1, courseID);
+            preparedStatement.setString(1, courseID);
             preparedStatement.setString(2, courseName);
             preparedStatement.setInt(3, credits);
-            preparedStatement.setInt(4, departmentID);
+            preparedStatement.setInt(4, SemesterID);
             preparedStatement.executeUpdate();
         } catch (Exception e) {
 
@@ -117,50 +106,81 @@ public class database {
         }
     }
 
-    private static void insertTeacherData(Connection connection, int teacherID, String firstName, String lastName, int age, int departmentID) {
-        String insertDataSQL = "INSERT INTO Teachers (TeacherID, FirstName, LastName, Age, DepartmentID) VALUES (?, ?, ?, ?, ?)";
+    private static void insertTeacherData(Connection connection, int teacherID, String firstName, String lastName, String sex, int departmentID) {
+        String insertDataSQL = "INSERT INTO Teachers (TeacherID, FirstName, LastName, Sex, DepartmentID) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertDataSQL)) {
             preparedStatement.setInt(1, teacherID);
             preparedStatement.setString(2, firstName);
             preparedStatement.setString(3, lastName);
-            preparedStatement.setInt(4, age);
+            preparedStatement.setString(4, sex);
             preparedStatement.setInt(5, departmentID);
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+    private static void insertSemesterData(Connection connection, int semesterID, String semesterName) {
+        String insertDataSQL = "INSERT INTO Semester (SemesterID, SemesterName) VALUES (?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertDataSQL)) {
+            preparedStatement.setInt(1, semesterID);
+            preparedStatement.setString(2, semesterName);
             preparedStatement.executeUpdate();
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }
     }
     public static void main(String[] args) {
-        createDatabase();
-        //displayStudents(url);
-        insertData(12,"hamdi","abdu",17);
-        // displayStudents(url);
+       // createDatabase();
+
         try {
             // Establish a database connection
-            Connection connection = DriverManager.getConnection(url);
+            Connection con = DriverManager.getConnection(url);
 
-            // Insert sample data into the Department table
-            insertDepartmentData(connection, 1, "Computer Science");
-            insertDepartmentData(connection, 2, "Mathematics");
-            // Add more departments as needed
 
-            // Insert sample data into the Students table
-            insertStudentData(connection, 101, "John", "Doe", 20, 1); // Student in Computer Science
-            insertStudentData(connection, 102, "Jane", "Smith", 22, 2); // Student in Mathematics
-            // Add more students as needed
+            insertDepartmentData(con, 1, "Computer Science");
+            insertDepartmentData(con, 2, "Mathematics");
+            insertDepartmentData(con, 3, "Physics");
 
-            // Insert sample data into the Courses table
-            insertCourseData(connection, 201, "Introduction to Programming", 3, 1); // Computer Science course
-            insertCourseData(connection, 202, "Calculus I", 4, 2); // Mathematics course
-            // Add more courses as needed
+            // Insert sample data for Students
+            insertStudentData(con, 1, "John", "Doe", "Male", 1);
+            insertStudentData(con, 2, "Jane", "Smith", "Female", 2);
+            insertStudentData(con, 3, "Bob", "Johnson", "Male", 1);
 
-            // Insert sample data into the Teachers table
-            insertTeacherData(connection, 301, "Professor", "Johnson", 40, 1); // Teacher in Computer Science
-            insertTeacherData(connection, 302, "Dr.", "Williams", 45, 2); // Teacher in Mathematics
-            // Add more teachers as needed
+            // Insert sample data for Courses
 
-            // Close the connection
-            connection.close();
+            insertCourseData(con, "C001", "Introduction to Programming", 3, 1);
+            insertCourseData(con, "C002", "Calculus I", 4, 1);
+            insertCourseData(con, "C003", "Physics 101", 4, 1);
+            insertCourseData(con, "C004", "Data Structures", 3, 1);
+            insertCourseData(con, "C005", "Linear Algebra", 4, 1);
+            insertCourseData(con, "C006", "Electricity and Magnetism", 4, 1);
+
+            insertCourseData(con, "C007", "Algorithms", 3, 2);
+            insertCourseData(con, "C008", "Differential Equations", 4, 2);
+            insertCourseData(con, "C009", "Optics", 4, 2);
+            insertCourseData(con, "C010", "Database Management Systems", 3, 2);
+            insertCourseData(con, "C011", "Number Theory", 4, 2);
+            insertCourseData(con, "C012", "Mechanics", 4, 2);
+
+            insertCourseData(con, "C013", "Software Engineering", 3, 3);
+            insertCourseData(con, "C014", "Probability and Statistics", 4, 3);
+            insertCourseData(con, "C015", "Thermodynamics", 4, 3);
+            insertCourseData(con, "C016", "Computer Networks", 3, 3);
+            insertCourseData(con, "C017", "Graph Theory", 4, 3);
+            insertCourseData(con, "C018", "Quantum Mechanics", 4, 3);
+
+            // Insert sample data for Teachers
+            insertTeacherData(con, 1, "Professor", "Smith", "Male", 1);
+            insertTeacherData(con, 2, "Professor", "Johnson", "Male", 2);
+            insertTeacherData(con, 3, "Professor", "Brown", "Male", 3);
+
+            // Insert sample data for Semesters
+            insertSemesterData(con, 1, "Fall 2022");
+            insertSemesterData(con, 2, "Spring 2023");
+            insertSemesterData(con, 3, "Summer 2023");
+
+
+            con.close();
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }

ec1625ca17fc824487d62d74364a05a9413affab
Fira0985 <firafisberhanu4@gmail.com>
2023-12-21 14:30:37 +0300

    update on spinner

---
+++ SGTS/src/main/java/chart/chartop/registerApp.java
@@ -0,0 +1,29 @@
+
+package chart.chartop;
+
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class registerApp extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(registerApp.class.getResource("register2.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 900, 760);
+        stage.setTitle("Course Adding");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+

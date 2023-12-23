c6caef0ea8beaf05c3c635f72505e2b841979c7c
yasinhusenwako <135891747+yasinhusenwako@users.noreply.github.com>
2023-12-23 20:15:31 +0200

    unit testing 2

--- SGTS/src/test/java/chart/chartop/UserTest.java
+++ SGTS/src/test/java/chart/chartop/UserTest.java
@@ -2,27 +2,50 @@ package chart.chartop;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
 
 class UserTest {
 
-    private User user; // Declare the User object to be tested
+    private User user;
 
     @BeforeEach
     void setUp() {
-        // Set up a User object for testing
-        user = new User("Example Course", 75);
+        // Create a User object with a score of 90 for setup
+        user = new User("Operating system", 90);
     }
 
     @Test
-    void getCourse() {
-        // Check if the course name fetched from the User object matches the expected value
-        assertEquals("Example Course", user.getCourse(), "Course name should match");
+    void calculateGrade() {
+        // Test if the grade is A for a score of 90
+        assertEquals(User.Grade.A, user.calculateGrade());
+
     }
 
     @Test
-    void getScore() {
-        // Verify if the score obtained from the User object is as expected
-        assertEquals(75, user.getScore(), "Score should be 75");
+    void getCourse () {
+        // Test if the course name is "Operating system"
+        assertEquals("Operating system", user.getCourse());
+    }
+
+    @Test
+    void getScore () {
+        // Test if the score is 90
+        assertEquals(90, user.getScore());
+    }
+
+    @Test
+    void getGrade () {
+        // Test if the grade is A
+        assertEquals(User.Grade.A, user.getGrade());
+    }
+
+    @Test
+    void getGradeValue () {
+        // Test if the grade value is 4 for A
+        assertEquals(4, user.getGradeValue());
+
     }
 }

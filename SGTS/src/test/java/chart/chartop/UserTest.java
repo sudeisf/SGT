c5af1e14e68802a7984c592e0a4e5ccb46d5932a
yasinhusenwako <135891747+yasinhusenwako@users.noreply.github.com>
2023-12-23 20:28:56 +0200

    unit testing3

--- SGTS/src/test/java/chart/chartop/UserTest.java
+++ SGTS/src/test/java/chart/chartop/UserTest.java
@@ -1,10 +1,11 @@
 package chart.chartop;
 
+//importing packages
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 
 
 class UserTest {
